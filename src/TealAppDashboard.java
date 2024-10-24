9613445cc206b4e44e16adc1b3fd6dc8f7992047
Aubrey <Poppyanimal@Yahoo.com>
2024-10-24 18:23:17 -0500

    merged app window changes to prevent errors

--- src/TealAppDashboard.java
+++ src/TealAppDashboard.java
@@ -1,7 +1,5 @@
-package com.example.tealtheappuserinteractions;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
