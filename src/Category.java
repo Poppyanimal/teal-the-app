5865519518557876132b20734339b92cdbb74d33
Aubrey <Poppyanimal@Yahoo.com>
2024-10-24 19:23:48 -0500

    simplified redundant categories code and removed example code
    also hooked up teal app launch to main class

---
+++ src/Category.java
@@ -0,0 +1,16 @@
+///import java.util.Locale.Category;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category
+{
+    public final String categoryID;
+    public String categoryName;
+
+    public Category(String id, String name)
+    {
+        categoryID = id;
+        categoryName = name;
+    }
+
+}
